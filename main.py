584da6b520278c4471ab59145bfeb4cde6bd976b
alibabajani <alibabajani310@gmail.com>
2025-12-24 10:07:49 +0330

    added "add student" to app

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import os
 
-print("wellcome to s-a")
-print("developed by alibabajani")
+print("wellcome to s-a.")
+print("developed by alibabajani.")
+
+def add_student():
+    name = input("name:")
+    student_code = input("student code:")
+    funds = input("funds:")
+    if os.path.exists(name):
+        open(f"{name}/{name}.txt", "w+")
+        open(f"{name}/{student_code}.txt", "w+")
+        open(f"{name}/{funds}.txt", "w+")
+        print("student sucssesfuly added.")
